af76dbd0c6a3e416b9a060108e4b841b49094bd3
Matilde Morrone <contact@morrone.dev>
2025-07-16 18:33:45 +0200

    Move startup code to a separate crate

--- libc/src/lib.rs
+++ libc/src/lib.rs
@@ -8,34 +8,7 @@ pub mod errno;
 pub mod mem;
 pub mod stdio;
 
-use core::{
-    arch::naked_asm,
-    ffi::{c_char, c_int},
-    ptr::null_mut,
-};
-
-use rustix;
-
 #[panic_handler]
 fn panic(_panic: &core::panic::PanicInfo<'_>) -> ! {
     core::intrinsics::abort()
-}
-
-unsafe extern "C" {
-    fn main(argc: c_int, argv: *mut *mut c_char, envp: *mut *mut c_char) -> c_int;
-}
-
-#[unsafe(naked)]
-#[unsafe(no_mangle)]
-pub unsafe extern "C" fn _start() -> ! {
-    fn entry(_mem: *mut usize) -> ! {
-        unsafe { rustix::runtime::exit_group(main(0, null_mut(), null_mut())) }
-    }
-
-    naked_asm!(
-        "mov rdi, rsp",
-        "push rbp",
-        "jmp {entry}",
-        entry = sym entry,
-    );
 }
---
+++ startup/src/lib.rs
@@ -0,0 +1,39 @@
+#![no_std]
+#![allow(internal_features)]
+#![feature(core_intrinsics)]
+#![feature(c_variadic)]
+#![feature(thread_local)]
+#![feature(linkage)]
+
+use core::{
+    arch::naked_asm,
+    ffi::{c_char, c_int},
+    ptr::null_mut,
+};
+
+use rustix;
+
+#[panic_handler]
+#[linkage = "weak"] // Nothing can panic here but rust won't compile without defining the symbol
+fn panic(_panic: &core::panic::PanicInfo<'_>) -> ! {
+    core::intrinsics::abort()
+}
+
+unsafe extern "C" {
+    fn main(argc: c_int, argv: *mut *mut c_char, envp: *mut *mut c_char) -> c_int;
+}
+
+#[unsafe(naked)]
+#[unsafe(no_mangle)]
+pub unsafe extern "C" fn _start() -> ! {
+    fn entry(_mem: *mut usize) -> ! {
+        unsafe { rustix::runtime::exit_group(main(0, null_mut(), null_mut())) }
+    }
+
+    naked_asm!(
+        "mov rdi, rsp",
+        "push rbp",
+        "jmp {entry}",
+        entry = sym entry,
+    );
+}
