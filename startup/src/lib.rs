cb1ac2cca91ac313e2eafcb81d4273af205634d3
Matilde Morrone <contact@morrone.dev>
2025-08-24 00:55:34 +0200

    Add envp parsing and more sanity checks

--- startup/src/lib.rs
+++ startup/src/lib.rs
@@ -4,8 +4,9 @@
 use core::{
     arch::naked_asm,
     ffi::{c_char, c_int},
-    ptr::null_mut,
 };
+
+use xenia::exit_group;
 
 #[panic_handler]
 #[linkage = "weak"] // Nothing can panic here but rust won't compile without defining the symbol
@@ -14,16 +15,23 @@ fn panic(_panic: &core::panic::PanicInfo<'_>) -> ! {
 }
 
 unsafe extern "C" {
-    fn main(argc: c_int, argv: *mut *mut c_char, envp: *mut *mut c_char) -> c_int;
+    safe fn main(argc: c_int, argv: *mut *mut c_char, envp: *mut *mut c_char) -> c_int;
 }
 
 fn entry(stack: *mut usize) -> ! {
-    unsafe {
-        let argc = *stack as c_int;
-        let argv: *mut *mut c_char = stack.offset(1).cast();
-
-        xenia::exit_group(main(argc, argv, null_mut()))
+    if stack.is_null() {
+        exit_group(1) // Something has gone horrifically wrong :(
     }
+
+    let (argc, argv, envp) = unsafe {
+        let argc = *stack as c_int;
+        let argv = stack.offset(1).cast::<*mut c_char>();
+        let envp = argv.offset(argc as isize + 1);
+
+        (argc, argv, envp)
+    };
+
+    xenia::exit_group(main(argc, argv, envp))
 }
 
 #[unsafe(naked)]
