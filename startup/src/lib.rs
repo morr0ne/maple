68a104152da8cb2f37403149825f87ad76823c97
Matilde Morrone <contact@morrone.dev>
2025-08-23 23:58:00 +0200

    Fix abi requirement for startup assembly

--- startup/src/lib.rs
+++ startup/src/lib.rs
@@ -17,17 +17,18 @@ unsafe extern "C" {
     fn main(argc: c_int, argv: *mut *mut c_char, envp: *mut *mut c_char) -> c_int;
 }
 
+fn entry(_mem: *mut usize) -> ! {
+    unsafe { xenia::exit_group(main(0, null_mut(), null_mut())) }
+}
+
 #[unsafe(naked)]
 #[unsafe(no_mangle)]
 pub unsafe extern "C" fn _start() -> ! {
-    fn entry(_mem: *mut usize) -> ! {
-        unsafe { xenia::exit_group(main(0, null_mut(), null_mut())) }
-    }
-
     naked_asm!(
-        "mov rdi, rsp",
-        "push rbp",
-        "jmp {entry}",
+        "xor rbp, rbp", // Clear frame pointer as required by the x86_64 abi
+        "mov rdi, rsp", // Pass the stack pointer to the enty function
+        "and rsp, -16", // Make sure the stack is 16 byte aligned
+        "call {entry}", // Use call instead of jmp to preserve a stack trace
         entry = sym entry,
     );
 }
